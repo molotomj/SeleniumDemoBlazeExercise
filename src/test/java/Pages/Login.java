7af3c36602ff5885d111ac7779036e79561c60f6
Veronica <veronicammabatho@yahoo.com>
2025-04-24 19:39:40 +0200

    Added the base class

--- src/test/java/Pages/Login.java
+++ src/test/java/Pages/Login.java
@@ -1,5 +1,35 @@
 package Pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class   Login {
 
+    WebDriver driver;
+
+    // Locators
+    By customerLoginButton = By.xpath("//button[contains(text(),'Customer Login')]");
+    By customerDropdown = By.id("userSelect");
+    By loginButton = By.xpath("//button[contains(text(),'Login')]");
+
+    // Constructor
+    public Login(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Methods
+    public void clickCustomerLogin() {
+        driver.findElement(customerLoginButton).click();
+    }
+
+    public void selectCustomer(String customerName) {
+        WebElement dropdown = driver.findElement(customerDropdown);
+        dropdown.click();
+        dropdown.findElement(By.xpath("//option[contains(text(),'" + customerName + "')]")).click();
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
 }
+
